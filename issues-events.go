6a79b72c0849433a39fed26bd4882cfb1fc41791
kyokomi <kyoko1220adword@gmail.com>
2014-08-02 21:15:52 +0900

    fix: projectNameとauthorNameを表示

--- issues-events.go
+++ issues-events.go
@@ -2,16 +2,16 @@ package main
 
 type IssuesEvents struct {
 	ObjectAttributes struct {
-		AssigneeID  float64     `json:"assignee_id"`
-		AuthorID    float64     `json:"author_id"`
+		AssigneeID  int64       `json:"assignee_id"`
+		AuthorID    int64       `json:"author_id"`
 		BranchName  interface{} `json:"branch_name"`
 		CreatedAt   string      `json:"created_at"`
 		Description string      `json:"description"`
-		ID          float64     `json:"id"`
-		Iid         float64     `json:"iid"`
+		ID          int64       `json:"id"`
+		Iid         int64       `json:"iid"`
 		MilestoneID interface{} `json:"milestone_id"`
-		Position    float64     `json:"position"`
-		ProjectID   float64     `json:"project_id"`
+		Position    int64       `json:"position"`
+		ProjectID   int64       `json:"project_id"`
 		State       string      `json:"state"`
 		Title       string      `json:"title"`
 		UpdatedAt   string      `json:"updated_at"`
--- server.go
+++ server.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"github.com/kyokomi/go-gitlab-client/gogitlab"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/binding"
 	"log"
+	"strconv"
 )
 
+const (
+	baseUrl = "https://gitlab.com"
+	apiPath = "/api/v3"
+	// TODO: あとで環境変数にする
+	token = "T5giqDC8dtz9ukx9D16B"
+)
 func main() {
+
+	gitlab := gogitlab.NewGitlab(baseUrl, apiPath, token)
+
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
+
+	m.Map(gitlab)
+
 	m.Post("/issues-events", binding.Bind(IssuesEvents{}), doIssuesEvents)
 	m.Run()
 }
 
-func doIssuesEvents(l *log.Logger, issues IssuesEvents) string {
+func doIssuesEvents(l *log.Logger, issues IssuesEvents, gitlab *gogitlab.Gitlab) string {
 
 	l.Println(issues)
 
+	// Project Get
+	project, err := gitlab.Project(strconv.FormatInt(issues.ObjectAttributes.ProjectID, 20))
+	if err != nil {
+		l.Println(err)
+		return "Error"
+	}
+	l.Println("プロジェクト名: ", project.Name)
+
+	// User Get Author
+	author, err := gitlab.User(strconv.FormatInt(issues.ObjectAttributes.AuthorID, 20))
+	if err != nil {
+		l.Println(err)
+		return "Error"
+	}
+	l.Println("作成者: ", author.Name)
+
+	// User Get Assignee
+	if issues.ObjectAttributes.AssigneeID != 0 {
+		assignee, err := gitlab.User(strconv.FormatInt(issues.ObjectAttributes.AssigneeID, 20))
+		if err != nil {
+			l.Println(err)
+			return "Error"
+		}
+		l.Println("担当者: ", assignee.Name)
+	}
+
+	l.Println("タイトル: ", issues.ObjectAttributes.Title)
+	l.Println("内容: ", issues.ObjectAttributes.Description)
+	l.Println("作成日: ", issues.ObjectAttributes.CreatedAt)
+	l.Println("ステータス: ", issues.ObjectAttributes.State)
+
 	return "OK"
 }
+
